4bb83335ba2db26fe41b09678ceb5a2add5ccd07
CJ Wald <CJWald888@gmail.com>
2025-07-28 19:38:06 -0400

    premake started

---
+++ CInput/Build-CInput.lua
@@ -0,0 +1,39 @@
+project "CInput"
+   kind "ConsoleApp"
+   language "C++"
+   cppdialect "C++20"
+   targetdir "Binaries/%{cfg.buildcfg}"
+   staticruntime "off"
+
+   files { "Source/**.h", "Source/**.cpp" }
+
+   includedirs
+   {
+      "../vendor/SDL"
+   }
+
+
+   targetdir ("../Binaries/" .. OutputDir .. "/%{prj.name}")
+   objdir ("../Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
+
+   filter "system:windows"
+       systemversion "latest"
+       defines { "WINDOWS" }
+
+   filter "configurations:Debug"
+       defines { "DEBUG" }
+       runtime "Debug"
+       symbols "On"
+
+   filter "configurations:Release"
+       defines { "RELEASE" }
+       runtime "Release"
+       optimize "On"
+       symbols "On"
+
+   filter "configurations:Dist"
+       kind "WindowedApp"
+       defines { "DIST" }
+       runtime "Release"
+       optimize "On"
+       symbols "Off"
